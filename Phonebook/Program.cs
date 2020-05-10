58d8814da2a3229dd001715700483c8549d35f5d
User <user.email@mail.org>
2020-05-11 00:31:03 +0300

    refactoring vol.1

--- Phonebook/Program.cs
+++ Phonebook/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Phonebook.Entities;
 using Phonebook.Data;
 using System;
-using System.Linq;
+using Phonebook.Views;
 
 namespace Phonebook
 {
@@ -9,39 +9,46 @@ namespace Phonebook
     {
         static void Main(string[] args)
         {
-            bool ProgramIsRunning = true;
-            var userRepository = new BaseRepository<User>();
-            var contactRepository = new BaseRepository<Contact>();
+            //bool ProgramIsRunning = true;
+            //var userRepository = new BaseRepository<User>();
+            //var contactRepository = new BaseRepository<Contact>();
 
-            while (ProgramIsRunning)
-            {
-                ConsoleKey step = UserInterface.HomeViewModel();
+            //while (ProgramIsRunning)
+            //{
+            //    ConsoleKey step = UserInterface.HomeViewModel();
 
-                if (step == ConsoleKey.E)
-                {
-                    ProgramIsRunning = false;
-                    Environment.Exit(0);
-                    continue;
-                }
+            //    if (step == ConsoleKey.E)
+            //    {
+            //        ProgramIsRunning = false;
+            //        Environment.Exit(0);
+            //        continue;
+            //    }
 
-                while (step == ConsoleKey.L)
-                {
-                    User loginUser = UserInterface.LoginViewModel(userRepository);
+            //    while (step == ConsoleKey.L)
+            //    {
+            //        User loginUser = UserInterface.LoginViewModel(userRepository);
 
-                    if (loginUser != null)
-                    {
-                        if (loginUser.IsAdmin.HasValue)
-                            UserInterface.AdminMenu(userRepository, contactRepository);
-                        else
-                            UserInterface.UserMenu(contactRepository);
-                    }
-                    else
-                    {
-                        Console.WriteLine(Environment.NewLine + "Press ESC to return or press any key to continue");
-                        step = Console.ReadKey(true).Key != ConsoleKey.Escape ? ConsoleKey.L : ConsoleKey.Escape;
-                    }
-                }
-            }
+            //        if (loginUser != null)
+            //        {
+            //            if (loginUser.IsAdmin.HasValue)
+            //                UserInterface.AdminMenu(userRepository, contactRepository);
+            //            else
+            //                UserInterface.UserMenu(contactRepository);
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine(Environment.NewLine + "Press ESC to return or press any key to continue");
+            //            step = Console.ReadKey(true).Key != ConsoleKey.Escape ? ConsoleKey.L : ConsoleKey.Escape;
+            //        }
+            //    }
+            //}
+
+            var homeMenu = new HomeView();
+            homeMenu.Show();
+
+            Console.Clear();
+            Console.WriteLine("Thanks for using Phonebook. Have a nice day!");
+            Console.ReadKey(true);
         }
     }
 }
---
+++ Phonebook/Views/LoginView.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Phonebook.Views
+{
+    public class LoginView
+    {
+        public void Show()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"Input your credentials");
+                Console.Write($"Username: ");
+                string username = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(username))
+                {
+                    Console.WriteLine("Please input valid password.");
+                    Console.ReadKey(true);
+                    continue;
+                }
+
+                string password = "";
+                Console.Write($"Password: ");
+                do
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        password += key.KeyChar;
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                        {
+                            password = password.Substring(0, (password.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                        else if (key.Key == ConsoleKey.Enter)
+                        {
+                            break;
+                        }
+                    }
+                } while (true);
+
+                // TODO: Get user from db and check if null
+                // if null: 
+
+                Console.WriteLine("Invalid username or password. User not found.");
+                Console.ReadKey(true);
+                //continue; uncomment this line after you check for null user
+
+                // if not null:
+                // check if logged user is admin:
+                var isAdmin = true; // perform real check on the real user
+                // uncomment these lines
+                //if (isAdmin)
+                //{
+                //    var adminView = new AdminView();
+                //    adminView.Show();
+                //}
+                //else
+                //{
+                //    var userView = new UserView();
+                //    userView.Show();
+                //}
+            }
+        }
+    }
+}
