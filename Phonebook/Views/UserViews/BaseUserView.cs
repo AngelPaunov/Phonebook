2f09fae0000057cf0349488ab6ac91fbe25b525d
Angel239200 <angel.paunov00@gmail.com>
2020-05-14 16:53:11 +0300

    Refactoring attempt

---
+++ Phonebook/Views/UserViews/BaseUserView.cs
@@ -0,0 +1,41 @@
+﻿using Phonebook.Entities;
+using Phonebook.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phonebook.Views.UserViews
+{
+    public class BaseUserView
+    {
+        public uint GetIdFromInput()
+        {
+            bool isUserIdNumber = uint.TryParse(Console.ReadLine(), out uint userInputId);
+
+            Console.WriteLine();
+
+            if (!isUserIdNumber)
+            {
+                Console.WriteLine("Please input positive number.");
+                Console.ReadKey();
+                return 0;
+            }
+
+            return userInputId;
+        }
+
+        public User GetUserById(uint userId) {
+            var userFromInput = new User(userId);
+
+            UserRepository userRepository = new UserRepository();
+            userFromInput = userRepository.ReadUser(userFromInput);
+
+            if (userFromInput == null)
+            {
+                Console.WriteLine("Invalid user id. User not found.");
+                Console.ReadKey(true);
+            }
+            return userFromInput;
+        }
+    }
+}
--- Phonebook/Views/UserViews/DeleteUserView.cs
+++ Phonebook/Views/UserViews/DeleteUserView.cs
@@ -4,35 +4,46 @@ using System;
 
 namespace Phonebook.Views.UserViews
 {
-    public class DeleteUserView
+    public class DeleteUserView:BaseUserView
     {
         public void Show()
         {
             Console.WriteLine();
             Console.Write("Input user's id to delete: ");
-            bool isUserIdNumber = uint.TryParse(Console.ReadLine(), out uint userInputId);
+            //bool isUserIdNumber = uint.TryParse(Console.ReadLine(), out uint userInputId);
 
-            Console.WriteLine();
+            //Console.WriteLine();
 
-            if (!isUserIdNumber)
+            //if (!isUserIdNumber)
+            //{
+            //    Console.WriteLine("Please input positive number.");
+            //    Console.ReadKey();
+            //    return;
+            //}
+            uint userInput = GetIdFromInput();
+            if (userInput < 1)
             {
-                Console.WriteLine("Please input positive number.");
-                Console.ReadKey();
                 return;
             }
 
-            var userFromInput = new User(userInputId);
+            //var userFromInput = new User(userInputId);
 
-            UserRepository userRepository = new UserRepository();
-            userFromInput = userRepository.ReadUser(userFromInput);
+            //UserRepository userRepository = new UserRepository();
+            //userFromInput = userRepository.ReadUser(userFromInput);
 
+            //if (userFromInput == null)
+            //{
+            //    Console.WriteLine("Invalid user id. User not found.");
+            //    Console.ReadKey(true);
+            //    return;
+            //}
+            var userFromInput = GetUserById(userInput);
             if (userFromInput == null)
             {
-                Console.WriteLine("Invalid user id. User not found.");
-                Console.ReadKey(true);
                 return;
             }
 
+            var userRepository = new UserRepository();
             userRepository.DeleteUser(userFromInput);
             Console.WriteLine("User has been deleted.");
             Console.ReadKey(true);
