421144d01b55de8e7a00c6ea258e7463969d9c4f
Angel239200 <angel.paunov00@gmail.com>
2020-05-15 17:54:30 +0300

    Dependency Injection

--- Phonebook/Program.cs
+++ Phonebook/Program.cs
@@ -1,4 +1,6 @@
-﻿using Phonebook.CSVRepositories;
+﻿using Autofac;
+using Phonebook.CSVRepositories;
+using Phonebook.Entities;
 using Phonebook.Views;
 using System;
 
@@ -6,18 +8,40 @@ namespace Phonebook
 {
     class Program
     {
+        private static IContainer Container;
+
         static void Main(string[] args)
         {
-            var userRepository = new CSVUserRepository();
-            var contactRepository = new CSVContactRepository();
-            var phoneRepository = new CSVPhoneRepository();
+            //var userRepository = new CSVUserRepository();
+            //var contactRepository = new CSVContactRepository();
+            //var phoneRepository = new CSVPhoneRepository();
 
-            var homeMenu = new HomeView(userRepository, contactRepository, phoneRepository);
+            //var homeMenu = new HomeView(userRepository, contactRepository, phoneRepository);
+
+            ConfigureContainer();
+            var homeMenu = ComposeObjects();
             homeMenu.Show();
 
             Console.Clear();
             Console.WriteLine("Thanks for using Phonebook. Have a nice day!");
             Console.ReadKey(true);
+        }
+
+        private static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<CSVUserRepository>().As<IUserRepository>().SingleInstance();
+            builder.RegisterType<CSVContactRepository>().As<IContactRepository>().SingleInstance();
+            builder.RegisterType<CSVPhoneRepository>().As<IPhoneRepository>().SingleInstance();
+
+            builder.RegisterType<HomeView>().SingleInstance();
+
+            Container = builder.Build();
+        }
+
+        private static HomeView ComposeObjects()
+        {
+            return Container.Resolve<HomeView>();
         }
     }
 }
